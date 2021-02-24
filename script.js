d93e39357c16416a89ba1db520e534eefc523280
Carlos Evelo <carlosevelo96@gmail.com>
2021-02-23 17:12:26 -0700

    created if blocks for each type of search

--- script.js
+++ script.js
@@ -6,22 +6,187 @@ document.getElementById("searchSubmit").addEventListener("click", function(event
     const url = "https://swapi.dev/api/";
 
     if (type === "") {
-        fetch(url + "root" + "?search=" + input)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(json) {
-                console.log(json);
-            })
+        //Make a pretty pop up with hidden html
+        window.alert("Must select a search type")
     }
     else {
-        fetch(url + type + "?search=" + input)
+        if (input === "") {
+            fetch(url + type + "?search=")
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                //Parse all data 
+            })
+        }
+        else {
+            fetch(url + type + "?search=" + input)
             .then(function(response) {
                 return response.json();
             })
             .then(function(json) {
                 console.log(json);
+                //Format the results json
+                if (type === "films") {
+                    //Title & episode id
+
+                    //Producer & Director
+
+                    //Release date
+
+                    //Opening crawl
+
+                    //Characters (fetch urls)
+
+                    //Planets (fetch urls)
+
+                    //Species (fetch urls)
+
+                    //Starships (fetch urls) 
+
+                    //Vehicles (fetch urls)
+
+                }
+                else if (type === "people") {
+                    //Name
+
+                    //Birth year
+
+                    //Eye color
+
+                    //Gender
+
+                    //Hair color
+
+                    //Height 
+
+                    //Mass
+
+                    //Skin color
+
+                    //Home world (fetch url)
+
+                    //Films (fetch urls)
+
+                    //Species (fetch urls)
+
+                    //Starships (fetch urls)
+
+                    //Vehicles (fetch urls)
+
+                }
+                else if (type === "planets") {
+                    //Name
+
+                    //Diameter
+
+                    //Rotation Period
+
+                    //Orbital period
+
+                    //Gravity
+
+                    //Population
+
+                    //Climate
+
+                    //Terrain
+
+                    //Surface water
+
+                    //Residents (fetch urls)
+
+                    //Films (fetch urls)
+
+                }
+                else if (type === "species") {
+                    //Name 
+
+                    //Classification
+                    
+                    //Designation
+
+                    //Average Height
+
+                    //Average life span
+
+                    //eye colors
+                    
+                    //hair colors
+
+                    //skin colors
+
+                    //language
+
+                    //homeworld (fetch urls)
+
+                    //people (fetch urls)
+
+                    //films (fetch urls)
+
+                }
+                else if (type === "starships") {
+                    //Name 
+
+                    //Model
+
+                    //Starship class
+
+                    //Manufacturer
+
+                    //Cost in credits
+
+                    //length
+
+                    //Crew
+
+                    //Passengers
+
+                    //Max atmospheric speed 
+
+                    //Hyperdrive rating
+
+                    //Max num of Megalights per hour (speed)
+
+                    //Cargo capacity
+
+                    //Consumables (time)
+
+                    //Films (fetch urls)
+
+                    //Pilots (fetch urls)
+
+                }
+                else if (type === "vehicles") {
+                    //Name
+
+                    //Model
+
+                    //Vehicle class
+
+                    //Manufacturer
+
+                    //Length
+
+                    //Cost in credits
+
+                    //crew
+
+                    //passengers
+
+                    //max atmoshperic speed
+
+                    //cargo capacity
+
+                    //consumables
+
+                    //films (fetch urls)
+
+                    //pilots (fetch urls)
+                    
+                }
             })
+        }
     }
    
 })
